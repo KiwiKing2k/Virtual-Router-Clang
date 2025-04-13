96794b5c8f2f5970a1d51e59a991b69aea7c11e6
KiwiKing2k <dinustefan04@gmail.com>
2025-04-13 22:06:05 +0300

    could this be it?

--- router.c
+++ router.c
@@ -81,7 +81,7 @@ void handle_icmp_echo(char* buf, size_t len, size_t interface)
 {
     printf("Ajungem in handle icmp");
     struct ether_hdr* eth_hdr = (struct ether_hdr*)buf;
-    struct ip_hdr* ip_hdr = (struct ip_hdr*)(buf + sizeof(struct ether_hdr));
+    struct ip_hdr* ip_header = (struct ip_hdr*)(buf + sizeof(struct ether_hdr));
     struct icmp_hdr* icmp_hdr = (struct icmp_hdr*)(buf + sizeof(struct ether_hdr) + sizeof(struct ip_hdr));
 
     // Type 8 Echo Request
@@ -93,11 +93,11 @@ void handle_icmp_echo(char* buf, size_t len, size_t interface)
         icmp_hdr->check = htons(checksum((uint16_t*)icmp_hdr, sizeof(struct icmp_hdr)));
         printf("Checksum in echo reply");
         // Update IP header
-        uint32_t temp_ip = ip_hdr->source_addr;
-        ip_hdr->source_addr = ip_hdr->dest_addr;
-        ip_hdr->dest_addr = temp_ip;
-        ip_hdr->checksum = 0;
-        ip_hdr->checksum = htons(checksum((uint16_t*)ip_hdr, sizeof(struct ip_hdr)));
+        uint32_t temp_ip = ip_header->source_addr;
+        ip_header->source_addr = ip_header->dest_addr;
+        ip_header->dest_addr = temp_ip;
+        ip_header->checksum = 0;
+        ip_header->checksum = htons(checksum((uint16_t*)ip_header, sizeof(struct ip_hdr)));
         printf("Updated IP header");
         // Update Ethernet header
         uint8_t temp_mac[6];
@@ -148,7 +148,7 @@ void handle_ttl_exceeded(char* buf, size_t len, size_t interface)
 
     memcpy((uint8_t*)new_icmp_hdr + sizeof(struct icmp_hdr), ip_hdr, sizeof(struct ip_hdr) + 8);
 
-    new_icmp_hdr->check = checksum((uint16_t*)new_icmp_hdr, sizeof(struct icmp_hdr) + sizeof(struct ip_hdr) + 8);
+    new_icmp_hdr->check = htons(checksum((uint16_t*)new_icmp_hdr, sizeof(struct icmp_hdr) + sizeof(struct ip_hdr) + 8));
 
     if (send_to_link(sizeof(struct ether_hdr) + sizeof(struct ip_hdr) + sizeof(struct icmp_hdr) + sizeof(struct ip_hdr) + 8, (char*)new_buf
                      , interface) < 0)
@@ -193,24 +193,24 @@ void handle_host_unreachable(char* buf, size_t len, size_t interface)
 
     memcpy((uint8_t*)new_icmp_hdr + sizeof(struct icmp_hdr), ip_hdr, sizeof(struct ip_hdr) + 8);
 
-    new_icmp_hdr->check = checksum((uint16_t*)new_icmp_hdr, sizeof(struct icmp_hdr) + sizeof(struct ip_hdr) + 8);
+    new_icmp_hdr->check = htons(checksum((uint16_t*)new_icmp_hdr, sizeof(struct icmp_hdr) + sizeof(struct ip_hdr) + 8));
 
     if (send_to_link(sizeof(struct ether_hdr) + sizeof(struct ip_hdr) + sizeof(struct icmp_hdr) + sizeof(struct ip_hdr) + 8, (char*)new_buf
                      , interface) < 0)
     {
         exit(EXIT_FAILURE);
     }
-    printf("Sent ICMP Time Exceeded error\n");
+    printf("Sent ICMP host unreachable\n");
 }
 
-int validate_and_update_ttl(struct ip_hdr* ip_hdr, char* buf, size_t len, size_t interface)
+int validate_and_update_ttl(struct ip_hdr* ip_header, char* buf, size_t len, size_t interface)
 {
     // Save the old checksum
-    uint16_t old_checksum = ntohs(ip_hdr->checksum);
-    ip_hdr->checksum = 0;
+    uint16_t old_checksum = ntohs(ip_header->checksum);
+    ip_header->checksum = 0;
 
     // Calculate the checksum
-    uint16_t calculated_checksum = checksum((uint16_t*)ip_hdr, sizeof(struct ip_hdr));
+    uint16_t calculated_checksum = checksum((uint16_t*)ip_header, sizeof(struct ip_hdr));
 
     // Check if the checksum is valid
     if (old_checksum != calculated_checksum)
@@ -220,18 +220,18 @@ int validate_and_update_ttl(struct ip_hdr* ip_hdr, char* buf, size_t len, size_t
         return 0;
     }
 
-    if (ip_hdr->ttl <= 1)
+    if (ip_header->ttl <= 1)
     {
         printf("TTL expired\n");
         // icmp ttl<1
         handle_ttl_exceeded(buf, len, interface);
         return 0;
     }
-    ip_hdr->ttl--;
+    ip_header->ttl--;
 
-    ip_hdr->checksum = 0;
+    ip_header->checksum = 0;
     //update checksum
-    ip_hdr->checksum = htons(checksum((uint16_t*)ip_hdr, sizeof(struct ip_hdr)));
+    ip_header->checksum = htons(checksum((uint16_t*)ip_header, sizeof(struct ip_hdr)));
 
     return 1;
 }
@@ -320,12 +320,13 @@ struct route_table_entry* lpm(struct node* root, uint32_t ip)
 }
 
 void handle_arp_message(size_t len, size_t interface, char* buf, queue* que, int* queue_len, struct arp_table_entry* arp_table, int*
-                        arp_table_len)
+                        arp_table_len, struct node* root)
 {
     struct arp_hdr* arp_header = (struct arp_hdr*)(buf + sizeof(struct ether_hdr));
     if (ntohs(arp_header->opcode) == 1)
     {
         //request
+        printf("Received ARP request\n");
         uint8_t mac[6];
         get_interface_mac(interface, mac);
         // now send reply
@@ -339,7 +340,7 @@ void handle_arp_message(size_t len, size_t interface, char* buf, queue* que, int
         // 2 values 1 cup
 
         printf("Replied arp to ip: %s\n", inet_ntoa(*(struct in_addr*)&arp_header->tprotoa));
-        printf("Replied arp with mac:");
+        printf("Replied arp with mac: ");
         for (int i = 0; i < 6; i++)
         {
             printf("%02x", mac[i]);
@@ -347,31 +348,53 @@ void handle_arp_message(size_t len, size_t interface, char* buf, queue* que, int
                 printf(":");
         }
         printf("\n");
-        struct ether_hdr* ether_header = (struct ether_hdr*)buf;
-        memcpy(ether_header->ethr_dhost, ether_header->ethr_shost, 6);
-        memcpy(ether_header->ethr_shost, mac, 6);
+        struct ether_hdr* eth_header = (struct ether_hdr*)buf;
+        memcpy(eth_header->ethr_dhost, eth_header->ethr_shost, 6);
+        memcpy(eth_header->ethr_shost, mac, 6);
         if (send_to_link(len, buf, interface) < 0) exit(EXIT_FAILURE);
     }
     else
     {
         //receive reply
+        printf("Receiving ARP reply\n");
         if ((*arp_table_len) > 6)
         {
             printf("Receiving more than 6 arp table entries\n");
             exit(EXIT_FAILURE);
         }
+        //also print these to arp_table.txt.new file
+        FILE* fp = fopen("arp_table.txt.new", "a");
+        if (fp == NULL)
+        {
+            printf("Error opening file\n");
+            exit(EXIT_FAILURE);
+        }
+        fprintf(fp, "%s %02x:%02x:%02x:%02x:%02x:%02x\n",
+                inet_ntoa(*(struct in_addr*)&arp_header->sprotoa),
+                arp_header->shwa[0], arp_header->shwa[1], arp_header->shwa[2],
+                arp_header->shwa[3], arp_header->shwa[4], arp_header->shwa[5]);
+        fclose(fp);
         arp_table[(*arp_table_len)].ip = arp_header->sprotoa;
         memcpy(arp_table[(*arp_table_len)].mac, arp_header->shwa, 6);
         (*arp_table_len)++;
+        printf("Dequeue after adding new arp entry\n");
+        int old_que_len=*queue_len;
         //start dequeing and checking then requeing
-        for (int i = 0; i < *queue_len; i++)
+        for (int i = 0; i < old_que_len; i++)
         {
-            char* buf = queue_deq(*que);
-            struct ether_hdr* eth_hdr = (struct ether_hdr*)buf;
-            struct ip_hdr* ip_hdr = (struct ip_hdr*)(buf + sizeof(struct ether_hdr));
-            struct icmp_hdr* icmp_hdr = (struct icmp_hdr*)(buf + sizeof(struct ether_hdr) + sizeof(struct ip_hdr));
-            struct route_table_entry* match = linear_best_match(ip_hdr->dest_addr);
-            int found=0;
+            char* new_buf = queue_deq(*que);
+            size_t new_len;
+            memcpy(&new_len, new_buf, sizeof(size_t));
+            new_buf = new_buf + sizeof(size_t);
+            printf("New len is %zu\n", new_len);
+            struct ether_hdr* eth_hdr = (struct ether_hdr*)new_buf;
+            struct ip_hdr* ip_header = (struct ip_hdr*)(new_buf + sizeof(struct ether_hdr));
+            struct route_table_entry* match = lpm(root, ip_header->dest_addr);
+            //print dest_addr in human readable
+            struct in_addr dest_addr;
+            dest_addr.s_addr = ip_header->dest_addr;
+            printf("Dest addr: %s\n", inet_ntoa(dest_addr));
+            int found = 0;
             for (int j = 0; j < *arp_table_len; j++)
             {
                 if (arp_table[j].ip == match->next_hop)
@@ -390,18 +413,20 @@ void handle_arp_message(size_t len, size_t interface, char* buf, queue* que, int
                             printf(":");
                     }
                     printf("\n");
-                    //maybe update checksum?
-                    icmp_hdr->check = 0;
-                    icmp_hdr->check = htons(checksum((uint16_t*)icmp_hdr, sizeof(struct icmp_hdr)));
-                    ip_hdr->checksum = 0;
-                    ip_hdr->checksum = htons(checksum((uint16_t*)ip_hdr, sizeof(struct ip_hdr)));
-                    if (send_to_link(len, buf, match->interface) < 0) exit(EXIT_FAILURE);
+                    if (send_to_link(new_len, new_buf, match->interface) < 0) exit(EXIT_FAILURE);
                     printf("Sent packet to interface %d\n", match->interface);
+                    printf("Packet size: %zu\n", new_len);
+                    printf("IPv4 size: %zu\n", sizeof(struct ip_hdr));
                     found++;
+                    (*queue_len)--;
                     break;
                 }
             }
-            if (!found) queue_enq(*que, buf);
+            if (!found)
+            {
+                new_buf = new_buf - sizeof(size_t);
+                queue_enq(*que, new_buf);
+            }
         }
     }
 }
@@ -453,7 +478,11 @@ int main(int argc, char* argv[])
     //
 
     queue message_queue = create_queue();
-    int queue_length = 1;
+    int queue_length = 0;
+
+    //init arp table
+    FILE* fp = fopen("arp_table.txt.new", "w");
+    fclose(fp);
 
     while (1)
     {
@@ -475,17 +504,26 @@ int main(int argc, char* argv[])
 
         printf("Received packet on interface %zu\n", interface);
 
+
         if (ntohs(eth_hdr->ethr_type) == ARP_ETHERTYPE)
         {
             printf("Handling ARP packet\n");
-            handle_arp_message(len, interface, buf, &message_queue, &queue_length, arp_table, &arp_table_len);
+            handle_arp_message(len, interface, buf, &message_queue, &queue_length, arp_table, &arp_table_len, root);
             continue;
         }
         if (ntohs(eth_hdr->ethr_type) != IPV4_ETHERTYPE)
         {
             printf("Ignored non-IPv4 packet, EtherType: 0x%04x\n", ntohs(eth_hdr->ethr_type));
+            continue;
         }
 
+        if (!validate_and_update_ttl(ip_header, buf, len, interface))
+        {
+            //also handles ICMP response if ttl <=0
+            printf("Packet dropped due to invalid checksum or TTL expiration\n");
+            continue;
+        }
+        printf("Am trecut de ttl\n");
 
         //check if it is for me
 
@@ -499,27 +537,14 @@ int main(int argc, char* argv[])
             continue;
         }
 
-        if (!validate_and_update_ttl(ip_header, buf, len, interface))
-        {
-            //also handles ICMP response if ttl <=0
-            printf("Packet dropped due to invalid checksum or TTL expiration\n");
-            continue;
-        }
-        printf("Am trecut de ttl\n");
         //lpm
         struct route_table_entry* match = lpm(root, ip_header->dest_addr);
         struct route_table_entry* linear_match = linear_best_match(ip_header->dest_addr);
 
         if (match == NULL)
         {
-            printf("No match found in by lpm lololo\n");
+            printf("No match found in by lpm\n");
             handle_host_unreachable(buf, len, interface);
-            continue;
-        }
-        if (linear_match == NULL)
-        {
-            printf("No match found in by linear search\n");
-            /*handle_host_unreachable(buf, len, interface);*/
             continue;
         }
 
@@ -554,6 +579,7 @@ int main(int argc, char* argv[])
         }*/
 
         //proper arp fuckery
+        printf("Received packet length: %zu\n", len);
         int found = 0;
         for (int i = 0; i <= arp_table_len; i++)
         {
@@ -581,10 +607,6 @@ int main(int argc, char* argv[])
                         printf(":");
                 }
                 printf("\n");
-                //update checksum
-                struct icmp_hdr* icmp_hdr = (struct icmp_hdr*)(buf + sizeof(struct ether_hdr) + sizeof(struct ip_hdr));
-                icmp_hdr->check = 0;
-                icmp_hdr->check = htons(checksum((uint16_t*)icmp_hdr, sizeof(struct icmp_hdr)));
                 if (send_to_link(len, buf, match->interface) < 0) exit(EXIT_FAILURE);
                 break;
             }
@@ -592,9 +614,13 @@ int main(int argc, char* argv[])
         if (!found)
         {
             send_arp_request(match);
-            char* alloc_buf = (char*)malloc(len);
+            printf("Size of len b4 malloc: %zu\n", len);
+            char* alloc_buf = (char*)malloc(len + sizeof(size_t));
+            memcpy(alloc_buf, &len, sizeof(size_t));
+            alloc_buf = alloc_buf + sizeof(size_t);
             memcpy(alloc_buf, buf, len);
-            queue_enq(message_queue, alloc_buf);
+            queue_length++;
+            queue_enq(message_queue, alloc_buf - sizeof(size_t));
         }
     }
 
